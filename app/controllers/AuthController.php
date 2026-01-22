fe271cee3c78cc367c3310059355464fae98900f
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-01-22 12:02:53 +0100

    Mise à jour des contrôleurs (authentification et utilisateurs)

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,6 +1,15 @@
 <?php
+    use App\core\BaseController;
+    class AuthController extends BaseController{
+        
+        public function login(){
+            $this->render('login');
+        }
 
-    
+        public function register(){
+            $this->render('register');
+        }
 
+    }
 
 ?>
--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,13 +1,51 @@
 <?php
-    class UserController{
+    class UserController extends BaseController {
         private $UserService ; 
+        private $authService;
 
         public function __construct(){
-            $this->UserService = new User();
+            $this->UserModel = new User();
+            $this->authService = new AuthService();
         }
 
         public function storeUser(){
-            $this->UserService->create($_POST);
+            $result = $authService->register($_POST);
+            if($result == "EMAIL_EXIST"){
+                $error = "Email déja existe";
+                $this->render($file,'register',compact('error'));
+                return;
+            }
+            if($_POST['role'] == "teacher")
+                header('Location: '.BASE_URL.'teacher/index');
+            else if($_POST['role'] == "student")
+                header('Location: '.BASE_URL.'student/index');
+            else echo "hadchi makayeeench";
+                
+        }
+
+
+        public function storeTeacher(){
+            $result = $authService->register($_POST,'teacher');
+            if($result == "EMAIL_EXIST"){
+                $error = "Email déja existe";
+                $this->render($file,'register',compact('error'));
+                return;
+            }
+
+            header('Location: '.BASE_URL.'teacher/index');    
+            exit;        
+        }
+
+        public function storeStudent(){
+            $result = $authService->register($_POST,'student');
+            if($result == "EMAIL_EXIST"){
+                $error = "Email déja existe";
+                $this->render($file,'register',compact('error'));
+                return;
+            }
+
+            header('Location: '.BASE_URL.'student/index');    
+            exit;        
         }
     }
 ?>
