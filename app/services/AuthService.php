beffceabc43ee9ea2c33e3cc2994d31debd7f66f
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-01-23 09:47:40 +0100

    refactor : Refactorisation du service d’authentification

--- app/services/AuthService.php
+++ app/services/AuthService.php
@@ -1,13 +1,17 @@
 <?php
 namespace App\services;
 
-
-use Exception;
+use App\core\BaseController;
 use App\models\User;
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+use Dotenv\Dotenv;
+
     class AuthService{
-        public User $UserModel;
-        public $StudentModel;
-        public  $TeacherModel;
+        private User $UserModel;
 
         public function __construct(){
             $this->UserModel = new User();
@@ -17,21 +21,21 @@ use App\models\User;
         public function login(string $email, string $password){
             $user = $this->UserModel->findUserByEmail($email);
             if(!$user)
-                {return['success' => false,'message' => 'Email incorrect'];
-            }
-            if(!password_verify($password,$user['password'])){
-                return['success' =>false,'message' => 'Mot de passe incorrect'];
-            }
+                throw new Exception("Email incorrect");
+            if(!password_verify($password,$user['password']))
+                throw new Exception("Mot de passe incorrect");
 
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['role'] = $user['role'];
             $_SESSION['email'] = $user['email'];
+
+            return $user;
             
         }
 
         public function register(string $nom , string $email, string $password,string $role){
-            $row = $this->UserModel->findUserByEmail($email);
-            if($row && $row['email'] === $email){
+            $user = $this->UserModel->findUserByEmail($email);
+            if($user){
                 throw new Exception("Email déja existe");
             }
             return $this->UserModel->create($nom , $email, $password , $role);
@@ -40,6 +44,48 @@ use App\models\User;
         public function currentUser(){
             if(!isset($_SESSION['user_id'])) return null;
             return $this->UserModel->findUserById($_SESSION['user_id']);
+        }
+
+        public function storeTeacher($nom, $email, $password){
+            return $this->register($nom, $email, $password,'teacher');
+        }
+
+        public function storeStudent($nom , $email){
+            $NoHash = substr(md5(rand()), 0, 8);
+            
+            $this->register($nom,$email,$NoHash,'student');
+            $this->sendEmail($email, $NoHash);
+            
+            return $NoHash;
+        }
+
+        public function sendEmail($email, $password){
+            $mail = new PHPMailer(true);
+            try {
+                $mail->SMTPAuth = true;
+                $mail->isSMTP();
+                $mail->Host = $_ENV['SMTP_HOST'];
+                $mail->Username = $_ENV['SMTP_USER'];
+                $mail->Password = $_ENV['SMTP_PASS'];
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
+                $mail->setFrom($_ENV['SMTP_USER'], $_ENV['SMTP_NAME']);
+                $mail->addAddress($email);
+                $mail->isHTML(true);
+                $mail->Subject = 'Vos identifiants Minerva';
+                $mail->Body = "
+                    <h2>Bienvenue sur Minerva !</h2>
+                    <p>Voici vos identifiants de connexion :</p>
+                    <p><strong>Email :</strong> $email</p>
+                    <p><strong>Mot de passe :</strong> $password</p>
+                    <p>Connectez-vous sur : Minerva</p>
+                ";
+
+                $mail->send();
+                return 'Email envoyé avec succès';
+            } catch (Exception $e) {
+                return "Erreur d envoi:" . $e->getMessage();
+            }
         }
 
         public function logout(){
